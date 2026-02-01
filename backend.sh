8ec973a0f4c8be63eda92353b6f0614d0a6baaab
saivishnu0627 <saivishnu0627@gmail.com>
2026-02-01 00:24:30 -0600

    functions

--- backend.sh
+++ backend.sh
@@ -48,10 +48,11 @@ fi
 mkdir -p /app &>>$LOGFILE
 VALIDATE $? "creating the app directory"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
 VALIDATE $? "Downloading backend code"
 
 cd /app
+rm -rf /app/*
 unzip /tmp/backend.zip &>>$LOGFILE
 VALIDATE $? "Extracted backend code"
 
